cf15a7a2a8bf996408a61767f366b789cb6d0975
alaed <alaeddinemansouri2@gmail.com>
2022-12-06 12:14:42 +0100

    first commit

--- src/main/java/com/project/un_site_de_planification_et_de_suivi_de_projets/repos/EquipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.un_site_de_planification_et_de_suivi_de_projets.repos;
-
-import com.project.un_site_de_planification_et_de_suivi_de_projets.entities.Equipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipeRepository extends JpaRepository<Equipe, Long> {
-}
